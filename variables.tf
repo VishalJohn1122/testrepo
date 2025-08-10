36fc731e07b8cfec3be2242e2344cb0f8d7acafb
VishalJohn1122 <vishalthatapudi1309@gmail.com>
2025-08-10 22:19:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@ variable "location" {
   default = "East US"
 }
 
+variable "subscription_id" {
+  default = "33ef9b7d-a911-4f94-b5af-ece5c659895e"
+}
+
+variable "tenant_id" {
+  default = "673186d0-7ebe-472d-b2f0-6bd1911b9b12"
+}
+
 variable "resource_group_name" {
   default = "TestRG"
 }
@@ -25,3 +33,4 @@ variable "admin_username" {
 variable "admin_password" {
   default = "Password@1234" # Use a secure password in production
 }
+
