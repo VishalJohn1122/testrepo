22dfd5042880a52ea8abc97f272cb9acb645cd17
VishalJohn1122 <vishalthatapudi1309@gmail.com>
2025-08-28 19:29:56 +0530

    Update outputs.tf
    hierarchy has been changed and applied

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,13 @@
 output "public_ip_address" {
-  value = azurerm_public_ip.Tst-public_ip.ip_address
+  value = azurerm_public_ip.tst-public_ip.ip_address
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.TestSA.name
+  value = azurerm_storage_account.testsa01.name
 }
 
 output "key_vault_uri" {
-  value = azurerm_key_vault.TestKV.vault_uri
+  value = azurerm_key_vault.testkv01.vault_uri
 }
+
 
